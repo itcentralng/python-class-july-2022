6d2c0e3551e8feaa6c39fd2b6a3e088fef56d029
Nasir Mustapha <34660309+mrteey@users.noreply.github.com>
2022-07-18 10:49:29 +0100

    Create jul18.py
    Write your answers in the issues section of this repository

---
+++ jul18.py
@@ -0,0 +1,9 @@
+"""
+Assignment
+
+Instruction: Write your answers in the issues section of this repository
+"""
+# What do you understand by programming
+# Write 3 5 steps programs, and vividly explain every step.
+# Do a google search and write something about python programming language
+# Give 10 examples of softwares written in python

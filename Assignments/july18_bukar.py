ebe6ea72d88a95b38c2e4d26ecfdb08dd073cab8
bllllackiee <bukarusman2009@gmail.com>
2022-07-21 09:58:37 +0100

    assignment

---
+++ Assignments/july18_bukar.py
@@ -0,0 +1,29 @@
+# NUMBER ONE
+#  Computer programming is the process that professionals use to write code that insstructs how a computer,
+# application or software program performs.
+#
+# NUMBER TWO
+#  Defining the problem
+#  planning the solution
+#  coding the program
+#  testing the program
+#   
+#
+# NUMBER 3
+# python is a high level,interpreted,general purpose programming language. its design philosophy 
+# emphasizes code readability with the use of significant indentation. 
+# python was designed by guido van rossum in the year 1991
+
+# NUMBER FOUR
+# spotify
+# itunes
+# quora
+# pinterest
+# netflix
+# uber
+# dropbox
+# django
+# flask
+# reddit
+
+

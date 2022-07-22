67541a42757e927551b5cbefa9822fed650e02c5
Nasir Mustapha <teboy.com@gmail.com>
2022-07-22 11:13:42 +0100

    strings and practice

---
+++ jul22a.py
@@ -0,0 +1,22 @@
+num1 = 36
+
+num2 = 26
+
+num3 = num1 + num2
+
+print(num1)
+print(num2)
+print(num3)
+
+num4 = num1 % num2
+
+print(num4)
+
+# FLOATS
+
+num5 = 3.14
+num6 = 6.28
+
+num7 = num5 + num6
+
+print(num7)
---
+++ jul22b.py
@@ -0,0 +1,28 @@
+# STRINGS
+
+first_name = "Bukar Khaleel"
+last_name = 'John Samuel'
+middle_name = """Habib"""
+
+a = "mouse"
+a.capitalize()
+
+first_name.casefold()
+
+first_name.count('Kha')
+
+print(first_name.endswith('l'))
+print(first_name.endswith('t'))
+print(first_name.upper())
+print(first_name.lower())
+
+print(first_name.replace('a', 'o'))
+
+t = "this is a word, it is a very long one, it is this"
+
+print(t.replace('is', 'at'))
+
+
+print(first_name.split('a'))
+
+print(len(first_name))

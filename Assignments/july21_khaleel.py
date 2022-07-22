7ac5facb5fcca3119130078ebcbefb9b119489aa
KHAL33L <dasukikhaleel@gmail.com>
2022-07-22 10:17:27 +0100

    july 21 assignment

---
+++ Assignments/july21_khaleel.py
@@ -0,0 +1,6 @@
+A particular kind of data itme, as designed by the values it can take, the programming language used, or the operations that can be performed on it.
+Intergers are any whole numbers rannging from negative to positive 
+A variable is a name assigned to data
+yes, no, maybe, acurate, wrong
+class, 
+operators used to assign the value to variables e.g =, +

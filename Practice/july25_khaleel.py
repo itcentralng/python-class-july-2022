08d1cc1efe8a29403e4b530aaab8e974282b1c15
KHAL33L <dasukikhaleel@gmail.com>
2022-07-25 11:22:20 +0100

    string practice

---
+++ Practice/july25_khaleel.py
@@ -0,0 +1,30 @@
+name = 'Ibrahim Khaleel Abba Dasuki'
+age =19
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[-1])
+print(name[4])
+print(name[5])
+print(name[6])
+
+print(name.count('l',3,5))
+
+print(name.center(20,))
+
+print(name.endswith('e'))
+
+print(name.find('a'))
+
+print('{} is the best football player to ever live at the age of {}.'.format(name,age))
+
+print('{} is the best football player to ever live at the age of {}.'.format(name.split(' ')[1], age))
+
+
+
+
+
+
+
+

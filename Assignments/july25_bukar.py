f77e5852618fd73f9cd6ff0f36960ace76a89aae
bllllackiee <bukarusman2009@gmail.com>
2022-07-26 12:08:26 +0100

    Changes

--- Assignments/july25_bukar.py
+++ Assignments/july25_bukar.py
@@ -1,5 +1,5 @@
 name = ('Umar')
 age = ('13')
-
+print(name[0] + name[-1])
 print(name.count('a'))
-print('hello {}, are you {} years old.'.format(name, age))
+print('hello {}, are you {} years old?'.format(name, age))
--- Practice/july25_bukar.py
+++ Practice/july25_bukar.py
@@ -1,5 +1,18 @@
-name = ('Umar')
+name = ('Abubakar Usman')
 age = ('13')
 
 print(name.count('a'))
 print('hello {}, are you {} years old.'.format(name, age))
+
+print(name[0:8])
+print(name[11:17])
+
+print(name.split()[0][:3]+name[11:17])
+print(name[:3]+name[11:17])
+
+print(name[:: -1])
+print(name[1 :: 4])
+
+number = 16
+num = '{}' .format(number)
+print(num.isdigit())

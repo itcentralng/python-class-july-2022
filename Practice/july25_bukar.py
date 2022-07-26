69f167b9a61bd0e9ff81e54a5480b7136b82ed33
bllllackiee <bukarusman2009@gmail.com>
2022-07-26 12:26:37 +0100

    changes

--- Practice/july25_bukar.py
+++ Practice/july25_bukar.py
@@ -16,3 +16,10 @@ print(name[1 :: 4])
 number = 16
 num = '{}' .format(number)
 print(num.isdigit())
+
+# WORKING WITH USER INPUT
+
+first_name = input('What is your first name ')
+second_name = input('What is your second name ')
+age = int(input('How old are you '))
+print("Hi {}, Nice to have you on educational app\nwe have 40 students with your last name {}\nand you will be {} years old in 5 years".format(first_name, second_name, age+5))

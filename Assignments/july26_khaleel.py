83b9b49db97af6133d87679828768e001169e979
KHAL33L <dasukikhaleel@gmail.com>
2022-07-27 14:00:08 +0100

    july 26 assignment

--- Assignments/july26_khaleel.py
+++ Assignments/july26_khaleel.py
@@ -1,14 +1,20 @@
 fruits = 'Apple,Mango,Guava,Orange,Pear'
  
-print(fruits[:5])
-print(fruits[6:11])
-print(fruits[12:17])
-print(fruits[18:24])
-print(fruits[25:])   
+#print(fruits[:5])
+#print(fruits[6:11])
+#print(fruits[12:17])
+#print(fruits[18:24])
+#print(fruits[25:])   
+
+print(fruits.split(',')[0])   
+print(fruits.split(',')[1])   
+print(fruits.split(',')[2])   
+print(fruits.split(',')[3])   
+print(fruits.split(',')[4])   
+  
 
 print(fruits[0:5]+fruits[25:])
 name= input('What is your first name?')
-job= input('What is your occupation?')
-age= int(input('How old are you?'))
+dob= 2022-int(input('What year were you born?'))
 
-print('Hello {}, you hve chosen {} as your job\ntherefore you shoulbe be {} years old.'.format(name, job, age))
+print('You are {} years old'.format(dob))

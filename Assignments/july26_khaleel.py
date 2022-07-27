cb600dd0584f4b6d65f14ead438a091f1f81c1e3
KHAL33L <dasukikhaleel@gmail.com>
2022-07-27 15:03:12 +0100

    july 26 assignment

--- Assignments/july26_khaleel.py
+++ Assignments/july26_khaleel.py
@@ -1,20 +1,26 @@
 fruits = 'Apple,Mango,Guava,Orange,Pear'
  
+#fruit1= fruits[:5]
+#fruit1= fruits.split(',')[0]
+#fruit1= fruits[::-1][-5:][::-1]
+fruit1= fruits[::-1][-24:][:6][::-1] #to print 'orange'
+print(fruit1)
+
 #print(fruits[:5])
 #print(fruits[6:11])
 #print(fruits[12:17])
 #print(fruits[18:24])
 #print(fruits[25:])   
 
-print(fruits.split(',')[0])   
-print(fruits.split(',')[1])   
-print(fruits.split(',')[2])   
-print(fruits.split(',')[3])   
-print(fruits.split(',')[4])   
+# print(fruits.split(',')[0])   
+# print(fruits.split(',')[1])   
+# print(fruits.split(',')[2])   
+# print(fruits.split(',')[3])   
+# print(fruits.split(',')[4])   
   
 
-print(fruits[0:5]+fruits[25:])
-name= input('What is your first name?')
-dob= 2022-int(input('What year were you born?'))
+# print(fruits[0:5]+fruits[25:])
+# name= input('What is your first name?')
+# dob= 2022-int(input('What year were you born?'))
 
-print('You are {} years old'.format(dob))
+# print('You are {} years old'.format(dob))

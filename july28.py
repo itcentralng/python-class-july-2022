27ee14229ceaf2dba9ca8977a11a9091a363e4d0
mmsalihawa <muskeedo4real@gmail.com>
2022-07-28 11:19:48 -0700

    july28 practice

---
+++ july28.py
@@ -0,0 +1,23 @@
+x = len("Hello")
+print(x)
+
+list2 = [[]]
+
+fruits = ['Apple', 'Orange', 'Banana']
+print(fruits[1])
+print(fruits[2][2:])
+print(fruits[1][2:].replace('n',''))
+print(fruits[1][2]+fruits[1][4:])
+
+#LIST METHODS
+
+# .append
+
+fruits.append('Mango')
+
+print(fruits)
+
+index = fruits.index('Apple')
+print(index)
+
+

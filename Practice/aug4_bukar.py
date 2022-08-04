f7666ee1b7654eebfe4c2bf123bee16df13c9ee3
bllllackiee <bukarusman2009@gmail.com>
2022-08-04 20:02:43 +0100

    changes

---
+++ Practice/aug4_bukar.py
@@ -0,0 +1,18 @@
+
+fruits = [
+    {"name": 'Uthman'},
+    {"Address": 'Unguwan Sarki'},
+    {"age":'13'},
+    {"Dob": "9/2/2009"},
+    {"place of birth": "Kaduna"},
+    {"work experince": "4 years"},
+    {"school attended": 'ILC'}
+    ]
+print("{}: ".format(fruits[0]['name']))
+print("{}: ".format(fruits[1]["Address"]))
+print("{}: ".format(fruits[2]["age"]))
+print("{}: ".format(fruits[3]["Dob"]))
+print("{}: ".format(fruits[4]["place of birth"]))
+print("{}: ".format(fruits[5]["work experince"]))
+print("{}: ".format(fruits[6]["school attended"]))
+

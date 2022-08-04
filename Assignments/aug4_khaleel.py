594b5ea3a51570527358ac655731ad55ca015912
KHAL33L <dasukikhaleel@gmail.com>
2022-08-04 22:05:59 +0100

    aug  4 assignment

---
+++ Assignments/aug4_khaleel.py
@@ -0,0 +1,5 @@
+me= {'name':'Khaleel Dasuki', 'address':'Umaru Guandu road', 
+        'number':'09038786136', 'age':20, 'dob':2002, 'pob':'Jeddah',
+            'work experience':'1 month', 'schools attented':'Al-Amanah Academy, Zamani College, Regent College, Newcastle University'}
+
+print('1. Name: {}\n2. Address: {}\n3. Phone Number: {}\n4. Age: {}\n5. Date of Birth: {}\n6. Place of Birth: {}\n7. Work Experience: {}\n8. Schools Attended: {}'.format(me['name'],me['address'],me['number'],me['age'],me['dob'],me['pob'],me['work experience'],me['schools attented']))

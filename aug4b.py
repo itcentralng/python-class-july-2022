8a8d6661f6343680200df6ab7e4ba24eb4850dd0
Nasir Mustapha <teboy.com@gmail.com>
2022-08-04 10:42:00 +0100

    more on dictionaries

---
+++ aug4b.py
@@ -0,0 +1,15 @@
+# Exercise
+
+"""
+1. Create a dictionary holding your personal information like; 
+    name, address, phone number, age, date of birth, place of birth, work experience, schools attended
+2. Print out your personal information in this format:
+    1. Name: John Doe
+    2. Address: 123 Main Street
+    3. Phone Number: 080-123-4567
+    4. Age: 20
+    5. Date of Birth: 12/12/2000
+    6. Place of Birth: Lagos
+    7. Work Experience: 1 year
+    8. Schools Attended: School of Science, School of Engineering, School of Business
+"""

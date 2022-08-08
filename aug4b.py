9fe4993bc61bd45af4ed4718527f4fb2873d9319
Nasir Mustapha <teboy.com@gmail.com>
2022-08-08 11:05:22 +0100

    more on dictionary and methods

--- aug4b.py
+++ aug4b.py
@@ -13,3 +13,27 @@
     7. Work Experience: 1 year
     8. Schools Attended: School of Science, School of Engineering, School of Business
 """
+
+# SOLUTIION
+
+personalinfo = {
+    "name":"Kyle Johnson",
+    "address": "39 Kuwait Crescent, Kaduna",
+    "phone":"+2349012345678",
+    "state":"Lagos",
+    "age":15,
+    "dob":"20-07-2007",
+    "pob":"Katsina",
+    "work":"5 Years",
+    "schools":["Darulhuda", "Command Secondary School", "Bayero University"]
+}
+print("\n")
+print(
+    """
+    1. Name: {}
+    2. Address: {}
+    3. Place of Birth: {}
+    4. Work Experience: {}
+    5. Schools Attended: My primary school is {}, my secondary school is {} and my university if {}
+    """.format(personalinfo["name"], personalinfo["address"], personalinfo["pob"], personalinfo["work"], personalinfo["schools"][0], personalinfo["schools"][1], personalinfo["schools"][2])
+)

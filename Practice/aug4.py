9ab65db43fbd406f836ada18d6a2d013c24589e7
mmsalihawa <muskeedo4real@gmail.com>
2022-08-08 04:25:43 -0700

    august4_mustapha.py

---
+++ Practice/aug4.py
@@ -0,0 +1,10 @@
+""""
+1. create a dictionary holding your personal information like;
+    name, age, date of birth, place of birth
+2. print out your information in this fo
+
+"""""
+
+Pinfo = {'name':'Mustapha', 'address':'kaduna', 'age':'20', 'dob':'9/9/2001', 'place of birth':'kaduna', 'work e':'1year', 'school':'SSS'}
+
+print (Pinfo['name'])

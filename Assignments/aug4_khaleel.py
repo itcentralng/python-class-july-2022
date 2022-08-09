dac94dce2d505a5ff4bf2cd656a0d30fdb5010c2
KHAL33L <dasukikhaleel@gmail.com>
2022-08-09 11:15:35 +0100

    minor changes

--- Assignments/aug4_khaleel.py
+++ Assignments/aug4_khaleel.py
@@ -1,5 +1,13 @@
-me= {'name':'Ibrahim Khaleel', 'address':'Umaru Guandu road', 
-        'number':'09038786136', 'age':20, 'dob':2002, 'pob':'Jeddah',
-            'work experience':'1 month', 'schools attented':'Al-Amanah Academy, Zamani College, Regent College, Newcastle University'}
+me= {'name':'Ibrahim Khaleel', 
+'address':'Umaru Guandu road', 
+'number':'09038786136', 
+'age':20, 
+'dob':2002, 
+'pob':'Jeddah',
+'work experience':'1 month', 
+'schools attented':'Al-Amanah Academy, Zamani College, Regent College, Newcastle University'}
 
 print('1. Name: {}\n2. Address: {}\n3. Phone Number: {}\n4. Age: {}\n5. Date of Birth: {}\n6. Place of Birth: {}\n7. Work Experience: {}\n8. Schools Attended: {}'.format(me['name'],me['address'],me['number'],me['age'],me['dob'],me['pob'],me['work experience'],me['schools attented']))
+
+# print('\n')
+# print(me['dob'])

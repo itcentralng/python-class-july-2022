a088d1344a0a4728ee8b57586c9a104898e2d544
KHAL33L <dasukikhaleel@gmail.com>
2022-08-15 10:11:58 +0100

    minor updates

--- Practice/aug8_khaleel.py
+++ Practice/aug8_khaleel.py
@@ -13,21 +13,21 @@ print(price)
 # . keys
 print(fruit.keys())
 # .clear     removes everything from the list
-fruit.clear()
-print(fruit)
-# .setdefault    returns the value of the specified key and 'insert the key, with the specified value' if the key doesnt exist
-print(fruit.setdefault("weight", "5 grams"))
-fruit["height"] = "5CM"
-print(fruit)
-# .update     adds the key value pair to the dictionary
-fruit.update(color="red")
-print(fruit)
-# .copy     returns a copy of the dictionary
-fruit1 = fruit.copy()
-print(fruit1)
-# .fromkeys     returns a dictionary with the chosen keys and values
-fruit2 = fruit.fromkeys(("name", ), "Pawpaw")
-print(fruit2)
-# .popitem      removes the last key-value pair
-print(fruit2.popitem())
-print(fruit2)
+# fruit.clear()
+# print(fruit)
+# # .setdefault    returns the value of the specified key and 'insert the key, with the specified value' if the key doesnt exist
+# print(fruit.setdefault("weight", "5 grams"))
+# fruit["height"] = "5CM"
+# print(fruit)
+# # .update     adds the key value pair to the dictionary
+# fruit.update(color="red")
+# print(fruit)
+# # .copy     returns a copy of the dictionary
+# fruit1 = fruit.copy()
+# print(fruit1)
+# # .fromkeys     returns a dictionary with the chosen keys and values
+# fruit2 = fruit.fromkeys(("name", ), "Pawpaw")
+# print(fruit2)
+# # .popitem      removes the last key-value pair
+# print(fruit2.popitem())
+# print(fruit2)

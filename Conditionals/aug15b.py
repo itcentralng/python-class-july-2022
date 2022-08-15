ef2406174dbc6eb4c7a13fe3ffad5b9c9383722c
Nasir Mustapha <teboy.com@gmail.com>
2022-08-15 11:07:04 +0100

    conditional assignment

---
+++ Conditionals/aug15b.py
@@ -0,0 +1,16 @@
+# CLASS EXERCISE ON CONDITIONALS
+
+"""
+1.) Using your understanding of conditionals, write a program that asks the user for a number.
+    If the number is greater than 0, print "The number is positive".
+    If the number is less than 0, print "The number is negative".
+    Otherwise print "The number is zero".
+
+    Hint: Use the if else if and else.
+
+2.) Using your understanding of conditionals, write another program that checks for the 
+    existence of a string in a list.
+    If the string is in the list, print "The string is in the list".
+    If the string is not in the list, print "The string is not in the list".
+    Hint: Use the if and else.
+"""

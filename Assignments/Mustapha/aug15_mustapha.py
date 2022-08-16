93b1df60aba352718d733ced718de32611332856
mmsalihawa <muskeedo4real@gmail.com>
2022-08-16 04:58:36 -0700

    changes

---
+++ Assignments/Mustapha/aug15_mustapha.py
@@ -0,0 +1,15 @@
+#2
+
+# # IF and ELSE
+fruits = ["apple", "banana", "grapes", "orange"]
+
+if "apple" in fruits:
+    print("apple is present in the string")
+else:
+    print("apple is not present in the string")
+if "mango" in fruits:
+    print("mango is present in the string")
+else:
+    print("mango is not present in the string")
+
+

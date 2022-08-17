f1b8f9359904ba37e4852e89fcee31c6253b6435
bllllackiee <bukarusman2009@gmail.com>
2022-08-17 09:15:10 +0100

    assignment

---
+++ Assignments/Bukar/aug16.bukar.py
@@ -0,0 +1,20 @@
+number = int(input('enter a number '))
+if number<=0:
+    print('negative')
+if number>=1:
+    print('positive')
+else:
+    print('Zero')  
+
+
+print('\n \n')
+
+fruits = ['Mango','Apple', 'Banana']
+if 'Apple' in fruits:
+    print('apple is present')
+else:
+    print('Apple is not present')
+if 'Grape' in fruits: 
+    print('Grape is present')
+else:
+    print('Grape is not preasent ')

234c4858b4964eb26acc381f3f48919fc4124c73
Nasir Mustapha <teboy.com@gmail.com>
2022-08-17 15:03:20 +0100

    intro to forloop

---
+++ Loops/aug17.py
@@ -0,0 +1,54 @@
+"""
+What is a loop?
+A loop is a sequence of statements that is repeated until a certain condition is met.
+An example is a loop that prints out the numbers from 1 to 10.
+"""
+
+#  There are two types of loops in Python:
+#  1. For loops
+#  2. While loops
+
+#  1. For loops
+"""
+A for loop is used to iterate over a sequence (e.g. a list) and execute 
+a block of code for each element.
+"""
+
+#  Syntax
+"""
+for <variable> in <iterable>:
+    <code to execute>
+"""
+
+#  Example
+
+students = ["John", "Paul", "George", "Ringo"]
+
+for s in students:
+    print('This student is named '+s)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sum = 0
+for n in numbers:
+    sum+=n
+print(sum)
+
+# Exercise
+"""
+1) Using for loop, write a program that prints out your name in this format:
+    N
+    a
+    m
+    e
+2) Using for loop, write a program that prints out the positive numbers from 
+    this list [1, -2, 3, -4, 5, -6, 7, -8, 9, -10]
+"""
+
+# Assignment
+
+"""
+Write a program that simulates a dictionary, a user can search for
+a word and the program will return what the word means. If the word does not 
+exist in the program then it will return "Word not found". The program should
+be case insensitive. The program should allow users to search for words 10 times only.
+"""

013bab2940bf80d1487dee1fed203a4cc1e7b192
mmsalihawa <muskeedo4real@gmail.com>
2022-08-18 02:26:05 -0700

    Changes

--- Assignments/Mustapha/aug17_mustapha.py
+++ Assignments/Mustapha/aug17_mustapha.py
@@ -1,9 +1,11 @@
 names = {'Mustapha':'small name', 'Khaleel':'big name', 'Bukar':'medium name'}
-
-meaning = input('Search the meaning of a name:')
-
 for n in names:
-    if meaning in names:
-        print('Mustapha {}\nKhaleel {}\nBukar {}\n'.format(names ['Mustapha'], names ['Khaleel'], names ['Bukar']))
+    meaning = input('Search the meaning of a name:')
+
+
+    if names.get(meaning):
+        print(names.get(meaning))
     else:
         print('Word not found')
+
+

8cd92f06204451602b8854fe2a3499c05989125d
blllackiee <bukarusman2009@gmail.com>
2022-08-22 11:11:37 +0100

    changes

--- Assignments/Bukar/july26_bukar.py
+++ Assignments/Bukar/july26_bukar.py
@@ -15,5 +15,5 @@ print(fruits[0:5]+fruits[29:])
 # Number Four
 first_name = input("What is your first name? ")
 
-print('you are '.format(first_name))
+print('you are {} '.format(first_name))
 
--- Data Types/String/jul22b.py
+++ Data Types/String/jul22b.py
@@ -4,28 +4,28 @@ from http.client import MULTI_STATUS
 
 
 first_name = "Bukar Khaleel"
-last_name = 'John Samuel'
-middle_name = """Habib"""
+# last_name = 'John Samuel'
+# middle_name = """Habib"""
 
-a = "mouse"
-a.capitalize()
+# a = "mouse"
+# a.capitalize()
 
-first_name.casefold()
+# first_name.casefold()
 
-first_name.count('Kha')
+# first_name.count('Kha')
 
-print(first_name.endswith('l'))
-print(first_name.endswith('t'))
-print(first_name.upper())
-print(first_name.lower())
+# print(first_name.endswith('l'))
+# print(first_name.endswith('t'))
+# print(first_name.upper())
+# print(first_name.lower())
 
-print(first_name.replace('a', 'o'))
+# print(first_name.replace('a', 'o'))
 
-t = "this is a word, it is a very long one, it is this"
+# t = "this is a word, it is a very long one, it is this"
 
-print(t.replace('is', 'at'))
+# print(t.replace('is', 'at'))
 
 
-print(first_name.split('a'))
+# print(first_name.split('a'))
 
 print(len(first_name))
